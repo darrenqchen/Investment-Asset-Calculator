bf56ca3a96c379f74e95c07cfddbf6d3fbf5dab5
Darren <darrenchen813@gmail.com>
2022-06-04 16:03:41 -0400

    Commented out the Sliders to prepare for the TextFields

--- src/InvestmentAssetCalculator.java
+++ src/InvestmentAssetCalculator.java
@@ -22,15 +22,15 @@ import javafx.stage.Stage;
 public class InvestmentAssetCalculator extends Application {
 
     private BorderPane mainLayout;
-    private BorderPane msLayout;
-    private Label msLabel;
-    private Slider msSlider;
-    private Label msTickLabel;
-    private BorderPane irLayout;
-    private Label irLabel;
-    private Slider irSlider;
-    private Label irTickLabel;
-    private VBox sliders;
+//    private BorderPane msLayout;
+//    private Label msLabel;
+//    private Slider msSlider;
+//    private Label msTickLabel;
+//    private BorderPane irLayout;
+//    private Label irLabel;
+//    private Slider irSlider;
+//    private Label irTickLabel;
+//    private VBox sliders;
     private NumberAxis xAxis;
     private NumberAxis yAxis;
     private LineChart<Number, Number> lineChart;
@@ -38,61 +38,61 @@ public class InvestmentAssetCalculator extends Application {
     public void init() {
         this.mainLayout = new BorderPane();
 
-        // monthly savings slider
-        this.msLayout = new BorderPane();
-
-        this.msLabel = new Label("Monthly savings");
-        msLayout.setLeft(msLabel);
-
-        this.msSlider = new Slider(25, 250, 25);
-        msLayout.setCenter(msSlider);
-        msSlider.setShowTickLabels(true);
-        msSlider.setShowTickMarks(true);
-        msSlider.setSnapToTicks(true);
-
-        this.msTickLabel = new Label("25");
-        msLayout.setRight(msTickLabel);
-
-        msSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
-            msTickLabel.setText(String.valueOf(newVal.intValue()));
-            lineChart.getData().clear();
-            lineChart.getData().addAll(getSavingsData(), getInterestData());
-        });
-
-        // yearly interest rate slider
-        this.irLayout = new BorderPane();
-
-        this.irLabel = new Label("Yearly interest rate");
-        irLayout.setLeft(irLabel);
-
-        this.irSlider = new Slider(0, 10, 0);
-        irLayout.setCenter(irSlider);
-        irSlider.setShowTickLabels(true);
-        irSlider.setShowTickMarks(true);
-        irSlider.setSnapToTicks(true);
-
-        this.irTickLabel = new Label("0");
-        irLayout.setRight(irTickLabel);
-
-        irSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
-            irTickLabel.setText(new DecimalFormat("#.##").format(newVal));
-            lineChart.getData().clear();
-            lineChart.getData().addAll(getSavingsData(), getInterestData());
-        });
-
-        // vbox sliders
-        this.sliders = new VBox();
-        mainLayout.setTop(sliders);
-        sliders.getChildren().addAll(msLayout, irLayout);
-
-        // line chart
-        this.xAxis = new NumberAxis(0, 30, 1);
-        this.yAxis = new NumberAxis();
-
-        this.lineChart = new LineChart<>(xAxis, yAxis);
-        lineChart.setTitle("Savings Calculator");
-
-        mainLayout.setCenter(lineChart);
+//        // monthly savings slider
+//        this.msLayout = new BorderPane();
+//
+//        this.msLabel = new Label("Monthly savings");
+//        msLayout.setLeft(msLabel);
+//
+//        this.msSlider = new Slider(25, 250, 25);
+//        msLayout.setCenter(msSlider);
+//        msSlider.setShowTickLabels(true);
+//        msSlider.setShowTickMarks(true);
+//        msSlider.setSnapToTicks(true);
+//
+//        this.msTickLabel = new Label("25");
+//        msLayout.setRight(msTickLabel);
+//
+//        msSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
+//            msTickLabel.setText(String.valueOf(newVal.intValue()));
+//            lineChart.getData().clear();
+//            lineChart.getData().addAll(getSavingsData(), getInterestData());
+//        });
+//
+//        // yearly interest rate slider
+//        this.irLayout = new BorderPane();
+//
+//        this.irLabel = new Label("Yearly interest rate");
+//        irLayout.setLeft(irLabel);
+//
+//        this.irSlider = new Slider(0, 10, 0);
+//        irLayout.setCenter(irSlider);
+//        irSlider.setShowTickLabels(true);
+//        irSlider.setShowTickMarks(true);
+//        irSlider.setSnapToTicks(true);
+//
+//        this.irTickLabel = new Label("0");
+//        irLayout.setRight(irTickLabel);
+//
+//        irSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
+//            irTickLabel.setText(new DecimalFormat("#.##").format(newVal));
+//            lineChart.getData().clear();
+//            lineChart.getData().addAll(getSavingsData(), getInterestData());
+//        });
+//
+//        // vbox sliders
+//        this.sliders = new VBox();
+//        mainLayout.setTop(sliders);
+//        sliders.getChildren().addAll(msLayout, irLayout);
+//
+//        // line chart
+//        this.xAxis = new NumberAxis(0, 30, 1);
+//        this.yAxis = new NumberAxis();
+//
+//        this.lineChart = new LineChart<>(xAxis, yAxis);
+//        lineChart.setTitle("Savings Calculator");
+//
+//        mainLayout.setCenter(lineChart);
     }
 
     public static void main(String[] args) {
@@ -106,27 +106,27 @@ public class InvestmentAssetCalculator extends Application {
         window.show();
     }
 
-    private XYChart.Series getSavingsData() {
-        XYChart.Series data = new XYChart.Series<>();
-        data.setName("Savings");
-        for (int i = 0; i <= 30; i += 1) {
-            data.getData().add(new XYChart.Data(i, 12 * i * Integer.parseInt(msTickLabel.getText())));
-        }
-        return data;
-    }
-
-    private XYChart.Series getInterestData() {
-        double num = 0;
-        DecimalFormat df = new DecimalFormat("#.###");
-        XYChart.Series data = new XYChart.Series<>();
-        data.setName("Interest");
-        for (int i = 0; i <= 30; i += 1) {
-            String temp = df.format((num + 12 * Integer.parseInt(msTickLabel.getText())) * Double.parseDouble(irTickLabel.getText()));
-            double doubleTemp = Double.parseDouble(temp);
-            data.getData().add(new XYChart.Data(i, doubleTemp));
-            num = doubleTemp;
-        }
-        return data;
-    }
+//    private XYChart.Series getSavingsData() {
+//        XYChart.Series data = new XYChart.Series<>();
+//        data.setName("Savings");
+//        for (int i = 0; i <= 30; i += 1) {
+//            data.getData().add(new XYChart.Data(i, 12 * i * Integer.parseInt(msTickLabel.getText())));
+//        }
+//        return data;
+//    }
+//
+//    private XYChart.Series getInterestData() {
+//        double num = 0;
+//        DecimalFormat df = new DecimalFormat("#.###");
+//        XYChart.Series data = new XYChart.Series<>();
+//        data.setName("Interest");
+//        for (int i = 0; i <= 30; i += 1) {
+//            String temp = df.format((num + 12 * Integer.parseInt(msTickLabel.getText())) * Double.parseDouble(irTickLabel.getText()));
+//            double doubleTemp = Double.parseDouble(temp);
+//            data.getData().add(new XYChart.Data(i, doubleTemp));
+//            num = doubleTemp;
+//        }
+//        return data;
+//    }
 
 }
