06f1885d58246501405771a73543e063cbec4f2a
Darren <darrenchen813@gmail.com>
2022-06-12 00:32:22 -0400

    Added the basic full functionality for the investment asset calculator.

--- src/InvestmentAssetCalculator.java
+++ src/InvestmentAssetCalculator.java
@@ -1,98 +1,100 @@
+/*
+TODO:
+- Make the TextFields only take in numbers
+- Make all the numbers go only to 3 decimal places
+ */
 
-
-
-import java.text.DecimalFormat;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.text.DecimalFormat;
 
 public class InvestmentAssetCalculator extends Application {
 
     private BorderPane mainLayout;
-//    private BorderPane msLayout;
-//    private Label msLabel;
-//    private Slider msSlider;
-//    private Label msTickLabel;
-//    private BorderPane irLayout;
-//    private Label irLabel;
-//    private Slider irSlider;
-//    private Label irTickLabel;
-//    private VBox sliders;
+    // Line Chart
     private NumberAxis xAxis;
     private NumberAxis yAxis;
     private LineChart<Number, Number> lineChart;
+    // Input Fields
+    private HBox inputFields;
+    // Starting Amount
+    private VBox startingAmount;
+    private Label startingAmountLabel;
+    private TextField startingAmountField;
+    // Yearly Contribution
+    private VBox monthlyContribution;
+    private Label monthlyContributionLabel;
+    private TextField monthlyContributionField;
+    // Rate of Return
+    private VBox ror;
+    private Label rorLabel;
+    private TextField rorField;
+    // Years to Grow
+    private VBox yearsToGrow;
+    private Label yearsToGrowLabel;
+    private TextField yearsToGrowField;
+    // Calculate Button
+    private Button calculateButton;
 
     public void init() {
         this.mainLayout = new BorderPane();
+        // Line Chart
+        this.xAxis = new NumberAxis();
+        xAxis.setLabel("Year");
+        this.yAxis = new NumberAxis();
+        yAxis.setLabel("Dollars ($)");
+        this.lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Savings Calculator");
+        mainLayout.setCenter(lineChart);
+        // Input Fields
+        this.inputFields = new HBox();
+        // Starting Amount
+        this.startingAmount = new VBox();
+        this.startingAmountLabel = new Label("Starting Amount:");
+        this.startingAmountField = new TextField();
+        startingAmount.getChildren().addAll(startingAmountLabel, startingAmountField);
+        // Monthly Contribution
+        this.monthlyContribution = new VBox();
+        this.monthlyContributionLabel = new Label("Monthly Contribution:");
+        this.monthlyContributionField = new TextField();
+        monthlyContribution.getChildren().addAll(monthlyContributionLabel, monthlyContributionField);
+        // Rate of Return
+        this.ror = new VBox();
+        this.rorLabel = new Label("Rate of Return:");
+        this.rorField = new TextField();
+        ror.getChildren().addAll(rorLabel, rorField);
+        // Years to Grow
+        this.yearsToGrow = new VBox();
+        this.yearsToGrowLabel = new Label("Years to Grow:");
+        this.yearsToGrowField = new TextField();
+        yearsToGrow.getChildren().addAll(yearsToGrowLabel, yearsToGrowField);
+        // Calculate Button
+        this.calculateButton = new Button("Calculate");
 
-//        // monthly savings slider
-//        this.msLayout = new BorderPane();
-//
-//        this.msLabel = new Label("Monthly savings");
-//        msLayout.setLeft(msLabel);
-//
-//        this.msSlider = new Slider(25, 250, 25);
-//        msLayout.setCenter(msSlider);
-//        msSlider.setShowTickLabels(true);
-//        msSlider.setShowTickMarks(true);
-//        msSlider.setSnapToTicks(true);
-//
-//        this.msTickLabel = new Label("25");
-//        msLayout.setRight(msTickLabel);
-//
-//        msSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
-//            msTickLabel.setText(String.valueOf(newVal.intValue()));
-//            lineChart.getData().clear();
-//            lineChart.getData().addAll(getSavingsData(), getInterestData());
-//        });
-//
-//        // yearly interest rate slider
-//        this.irLayout = new BorderPane();
-//
-//        this.irLabel = new Label("Yearly interest rate");
-//        irLayout.setLeft(irLabel);
-//
-//        this.irSlider = new Slider(0, 10, 0);
-//        irLayout.setCenter(irSlider);
-//        irSlider.setShowTickLabels(true);
-//        irSlider.setShowTickMarks(true);
-//        irSlider.setSnapToTicks(true);
-//
-//        this.irTickLabel = new Label("0");
-//        irLayout.setRight(irTickLabel);
-//
-//        irSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
-//            irTickLabel.setText(new DecimalFormat("#.##").format(newVal));
-//            lineChart.getData().clear();
-//            lineChart.getData().addAll(getSavingsData(), getInterestData());
-//        });
-//
-//        // vbox sliders
-//        this.sliders = new VBox();
-//        mainLayout.setTop(sliders);
-//        sliders.getChildren().addAll(msLayout, irLayout);
-//
-//        // line chart
-//        this.xAxis = new NumberAxis(0, 30, 1);
-//        this.yAxis = new NumberAxis();
-//
-//        this.lineChart = new LineChart<>(xAxis, yAxis);
-//        lineChart.setTitle("Savings Calculator");
-//
-//        mainLayout.setCenter(lineChart);
+
+
+        inputFields.getChildren().addAll(startingAmount, monthlyContribution, ror, yearsToGrow, calculateButton);
+        mainLayout.setTop(inputFields);
+        calculateButton.setOnAction((event) -> {
+            lineChart.getData().clear();
+            XYChart.Series data = addToChart(Double.parseDouble(startingAmountField.getText()), Double.parseDouble(monthlyContributionField.getText()), Double.parseDouble(rorField.getText()), Integer.parseInt(yearsToGrowField.getText()));
+            lineChart.getData().add(data);
+                }
+        );
     }
 
     public static void main(String[] args) {
@@ -106,27 +108,20 @@ public class InvestmentAssetCalculator extends Application {
         window.show();
     }
 
-//    private XYChart.Series getSavingsData() {
-//        XYChart.Series data = new XYChart.Series<>();
-//        data.setName("Savings");
-//        for (int i = 0; i <= 30; i += 1) {
-//            data.getData().add(new XYChart.Data(i, 12 * i * Integer.parseInt(msTickLabel.getText())));
-//        }
-//        return data;
-//    }
-//
-//    private XYChart.Series getInterestData() {
-//        double num = 0;
-//        DecimalFormat df = new DecimalFormat("#.###");
-//        XYChart.Series data = new XYChart.Series<>();
-//        data.setName("Interest");
-//        for (int i = 0; i <= 30; i += 1) {
-//            String temp = df.format((num + 12 * Integer.parseInt(msTickLabel.getText())) * Double.parseDouble(irTickLabel.getText()));
-//            double doubleTemp = Double.parseDouble(temp);
-//            data.getData().add(new XYChart.Data(i, doubleTemp));
-//            num = doubleTemp;
-//        }
-//        return data;
-//    }
+    private XYChart.Series addToChart(double startingAmount, double monthlyContribution, double ror, int yearsToGrow) {
+        double num = 0;
+        DecimalFormat df = new DecimalFormat("#.###");
+        XYChart.Series data = new XYChart.Series<>();
+        data.setName("Assets");
+        for (int i = 0; i <= yearsToGrow; i += 1) {
+            double rateOfReturn = (Double.parseDouble(rorField.getText()) / 100) + 1;
+            String temp = df.format((num + 12 * Integer.parseInt(monthlyContributionField.getText())) * rateOfReturn);
+            double doubleTemp = Double.parseDouble(temp);
+            data.getData().add(new XYChart.Data(i, doubleTemp));
+            num = doubleTemp;
+        }
+        return data;
+
+    }
 
 }
